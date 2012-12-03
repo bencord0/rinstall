ede13fbe3e1132c5b1bfe7642c327da255b434f8
Ben Cordero <bmc@linux.com>
2012-12-03 01:24:09 +0000

    Fix the u-boot build

--- mkuboot.sh
+++ mkuboot.sh
@@ -10,6 +10,7 @@ MAKEOPTS+="-j${PARALLEL}"
 # U-Boot stuff
 # Assume github://gonzoua/u-boot-pi (rpi branch) under ./u-boot
 pushd "${CURRENT_DIR}"/u-boot
+make ${MAKEOPTS} rpi_b_config
 make ${MAKEOPTS}
 popd
 
